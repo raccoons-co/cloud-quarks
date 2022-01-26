94ac9a1521ee77942337dd4945afa7dbc012ef1b
Oleksii Kucheruk <oleksii.kucheruk+github@teamdev.com>
2022-01-26 11:18:51 +0200

    Improve code readability

--- aws-cdk/install.sh
+++ aws-cdk/install.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
-set -e
-
 # This script is meant for quick & easy install.sh via:
 #   $ curl -fsSL https://raw.githubusercontent.com/raccoons-co/cloud-quarks/main/aws-cdk/install.sh | sudo sh
-get_distribution() {
+
+# Sets shell option "Exit immediately if a command exits with a non-zero status."
+set -e
+
+# Returns Linux distribution name String
+linuxDistributionName() {
   lsb_dist=""
   if [ -r /etc/os-release ]; then
     # shellcheck disable=SC1091
 	  lsb_dist=$(. /etc/os-release && echo "$ID")
   fi
-    echo "$lsb_dist"
+  echo "$lsb_dist"
 }
 
+# Returns same String with all letters in lowercase
 toLowerCase() {
   tr '[:upper:]' '[:lower:]'
 }
 
+# Installs AWS CDK and Maven to AmazonLinux, Debian, macOS.
 do_install(){
 
-    os=$( uname -s | toLowerCase )
-    case $os in
+    currentOsName=$( uname -s | toLowerCase )
+    case $currentOsName in
 	    linux)
-	      lsb_dist=$( get_distribution | toLowerCase )
+	      lsb_dist=$( linuxDistributionName | toLowerCase )
 	      if [ "$lsb_dist" = "ubuntu" ] || [ "$lsb_dist" = "debian" ]; then
 	        npm install -g aws-cdk
 		      sudo apt-get update
@@ -38,4 +43,6 @@ do_install(){
     esac
 }
 
+# Run installation process.
+# Last line in script file to protect during "curl | sh".
 do_install
